ea094c4c0c1ba42e01dda6fd59710839c2b8f38a
Sherwin  paponette <mystall05@gmail.com>
2026-05-12 14:34:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,6 @@
-const CACHE_NAME = 'g2-v1';
-const urlsToCache = ['./','./index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+REPOSITORY DISABLED
+Account compromised via identity theft prior to Sept 2024.
+FTC IdentityTheft.gov Report #201589169 filed.
+CFPB complaint against Stripe, Inc. pending for withheld funds.
+This public repo contains no source code, trade secrets, or evidence.
+All records witnesses preserved offline for legal proceedings.
